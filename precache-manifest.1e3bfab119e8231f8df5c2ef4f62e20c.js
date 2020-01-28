05f5d8e2ce2f1f44183b644b72b3aaef76160881
Arul <aruleeswaran@gmail.com>
2020-01-28 22:52:46 +0530

    testing if react works

---
+++ precache-manifest.1e3bfab119e8231f8df5c2ef4f62e20c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e654f52c560c85f039ad45d2ad3f228",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2561a210124a59d66043",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ae0faef99ba4ce85feae",
+    "url": "/static/js/2.4792ac41.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.4792ac41.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2561a210124a59d66043",
+    "url": "/static/js/main.3dd15b03.chunk.js"
+  },
+  {
+    "revision": "841dc5719ea15463dddd",
+    "url": "/static/js/runtime-main.eb4ca92d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
